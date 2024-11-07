061ab0dc41b14b684b1507e1fc3efc23c6c8d636
GabrielCaspirro <gabriel.gcd08@gmail.com>
2024-11-07 09:04:29 -0300

    ta funfando um pouco já

--- SiteCurriculo/app.js
+++ SiteCurriculo/app.js
@@ -1,37 +1,63 @@
 //Importando um monte de pacotes, se não dificulta bastante fazer o link de todos os arquivos html, js, etc.
 const express = require('express');
-const { VerificaLogin, AddCandidato } = require('./database/functionsSql'); 
+const { VerificaLogin, VerificaLoginAdmin, AddCandidato, AddEmpresa, VerificaEmpresa } = require('./database/functionsSql'); 
 const app = express();
 const path = require('path');
 app.use(express.urlencoded({ extended: true })); 
 //Essa função aqui faz os css's funcionarem
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'telas')));
 
 //IMPORTANTE: essas funções que tem "app.get" são pra fazer o link de html. Então sempre que alguém fizer um arquivo html novo 
 //precisa adicionar mais uma função semelhante a essa pra fazer a conexão corretamente entre os arquivos
 //Nessa função a gente tá dizendo que sempre que a rota da página for só "/", a gente vai ser levado pro index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/telas/index.html'));
 });
 //Rota do login
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/login.html');
+    res.sendFile(__dirname + '/telas/login.html');
 });
 //Rota do cadastro
 app.get('/cadastro', (req, res) => {
-    res.sendFile(__dirname + '/cadastro.html');
+    res.sendFile(__dirname + '/telas/cadastro.html');
+});
+//Rota do tipo do cadastro
+app.get('/tipo-cadastro', (req, res) => {
+    res.sendFile(__dirname + '/telas/empresa-ou-candidato.html');
+});
+app.get('/cadastro-empresa', (req, res) => {
+    res.sendFile(__dirname + '/telas/cadastro-empresa.html');
 });
 
 //Nessa função a gente tá basicamente pegando os dados que são enviados pelo forms da página de login e puxando a função de verificação
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-    //A funcão de verificação
+
     VerificaLogin(username, password)
         .then((user) => {
             if (user) {
-                res.send('Login bem-sucedido!'); // Login bem-sucedido
+                res.redirect('/'); // Login bem-sucedido para usuário comum
             } else {
-                res.send('Nome de usuário ou senha incorretos.'); // Login falhou
+                // Tenta login de administrador apenas se o login de usuário comum falhar
+                return VerificaLoginAdmin(username, password);
+            }
+        })
+        .then((admin) => {
+            if (admin) {
+                res.redirect('/'); // Login bem-sucedido para admin
+            } else if (admin === false || admin === null) {
+                // Se o login de admin falhar, tenta login de empresa
+                return VerificaEmpresa(username, password);
+            }
+        })
+        .then((empresa) => {
+            if (empresa) {
+                res.redirect('/'); // Login bem-sucedido para empresa
+            } else if (empresa === false || empresa === null) {
+                // Codifique o parâmetro e redirecione para a página de login
+                const errorMsg = encodeURIComponent('Nome de usuário ou senha incorretos!');
+                res.redirect(`/login?error=${errorMsg}`);
             }
         })
         .catch((err) => {
@@ -57,9 +83,10 @@ app.post('/cadastro', async (req, res) => {
     try {
         const result = await AddCandidato(novoCandidato);
         if (result.success) {
-            res.redirect('/'); // Redireciona para a página inicial ou onde desejar
+            res.redirect('/login'); // Redireciona para a página inicial ou onde desejar
         } else {
-            res.status(400).send(result.message); // Mensagem de erro específica
+            const errorMsg = encodeURIComponent(result.message);
+            res.redirect(`/cadastro?error=${errorMsg}`); // Mensagem de erro específica
         }
     } catch (err) {
         console.error(err);
@@ -67,6 +94,32 @@ app.post('/cadastro', async (req, res) => {
     }
 });
 
+app.post('/cadastro-empresa', async (req, res) => {
+    const { username, cnpj, telefone, email, password } = req.body;
+    const time = new Date();
+    
+    const novaEmpresa = {
+        nome: username,
+        cnpj,
+        telefone,
+        email,
+        senha: password,
+        data_cadastro: time
+    };
+
+    try {
+        const result = await AddEmpresa(novaEmpresa);
+        if (result.success) {
+            res.redirect('/login'); // Redireciona para a página inicial ou onde desejar
+        } else {
+            const errorMsg = encodeURIComponent(result.message);
+            res.redirect(`/cadastro-empresa?error=${errorMsg}`); // Mensagem de erro específica
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro no servidor');
+    }
+});
 
 // Inicializa o servidor
 app.listen(3000, () => {
