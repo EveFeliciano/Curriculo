63ba5a56c0edb3f76894f512648555f767d82bb4
Gabriel Caspirro Demarchi <124791810+GabrielCaspirro@users.noreply.github.com>
2024-11-20 23:02:57 -0300

    Add files via upload

--- curriculo.sql
+++ curriculo.sql
@@ -42,7 +42,7 @@ CREATE TABLE `administrador` (
 --
 
 INSERT INTO `administrador` (`id_administrador`, `nome`, `email`, `senha`, `data_cadastro`) VALUES
-(1, 'Gabriel', 'gabriel@email.com', '1234', '2024-11-05 00:00:00');
+(1, 'Administrador', 'admin@email.com', '1234', '2024-11-05 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -67,8 +67,7 @@ CREATE TABLE `candidato` (
 --
 
 INSERT INTO `candidato` (`id_candidato`, `nome`, `data_nasc`, `cpf`, `email`, `telefone`, `senha`, `data_cadastro`, `curriculo`) VALUES
-(1, 'Dalva', '2024-11-01', '244.244.343', 'dalva.casp65@gmail.com', '(11) 93031-5532', '1234', '2024-11-05 19:09:58', NULL),
-(8, 'GabrielCD', '2024-07-05', '244.256.343', 'gabriel.demarchi@etec.sp.gov.br', '(11) 93269-8014', '1234', '2024-11-08 20:19:08', 'uploads\\1731108293652.docx');
+(1, 'Candidato1', '2024-11-01', '244.244.343-01', 'candidato1@gmail.com', '(11) 93031-5532', '1234', '2024-11-05 19:09:58', NULL),
 
 -- --------------------------------------------------------
 
@@ -89,15 +88,6 @@ CREATE TABLE `candidatura` (
 -- Despejando dados para a tabela `candidatura`
 --
 
-INSERT INTO `candidatura` (`id_candidatura`, `data_candidatura`, `status`, `feedback`, `id_vaga`, `curriculo`) VALUES
-(2, '2024-11-09 00:07:11', 'aprovado', 'respondido', 9, 'dsydkisygvlisc'),
-(3, '2024-11-19 22:15:41', 'aprovado', 'respondido', 6, 'dssssssssssssssssssss'),
-(4, '2024-11-09 00:10:50', 'em espera', NULL, 10, 'aaaaaaaaaaaaa'),
-(5, '2024-11-09 00:14:50', 'reprovado', 'respondido', 9, 'aaaaaaaaa'),
-(6, '2024-11-09 11:45:21', 'aprovado', 'respondido', 14, 'Eu sei desenhar.');
-
--- --------------------------------------------------------
-
 --
 -- Estrutura para tabela `empresa`
 --
@@ -117,8 +107,7 @@ CREATE TABLE `empresa` (
 --
 
 INSERT INTO `empresa` (`id_empresa`, `nome`, `cnpj`, `telefone`, `email`, `senha`, `data_cadastro`) VALUES
-(1, 'Auau', '13.122.122/122', '(11) 93069-8014', 'gabriel.gcd08@gmail.com', '1234', '2024-11-07 17:51:09'),
-(2, 'UIIIII', '13.123.122/122', '(11) 93031-5532', 'gabriel.gc08@gmail.com', '1234', '2024-11-07 18:53:39');
+(1, 'Empresa1', '13.122.122/122', '(11) 93069-8014', 'empresa@email.com', '1234', '2024-11-07 17:51:09'),
 
 -- --------------------------------------------------------
 
@@ -130,17 +119,6 @@ CREATE TABLE `inscreve` (
   `id_candidatura` int(11) NOT NULL,
   `id_candidato` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `inscreve`
---
-
-INSERT INTO `inscreve` (`id_candidatura`, `id_candidato`) VALUES
-(2, 8),
-(3, 8),
-(4, 8),
-(5, 8),
-(6, 8);
 
 -- --------------------------------------------------------
 
@@ -163,16 +141,12 @@ CREATE TABLE `vaga` (
 --
 -- Despejando dados para a tabela `vaga`
 --
-
 INSERT INTO `vaga` (`id_vaga`, `titulo`, `cidade`, `descricao`, `data_publicacao`, `data_fechamento`, `estado`, `id_empresa`, `categoria`) VALUES
-(6, 'AngraMoments', 'aaaaaaaaaaaaaaaa', 'aaaaaiosssssssss', '2024-11-07 21:45:15', '2024-11-30 00:00:00', 'aaaaaaaaaaa', 1, 'design'),
-(7, 'escritor', 'ribeirao', 'teurfie7tfieuyfç', '2024-11-07 23:39:28', '2024-11-29 00:00:00', 'sp', 1, 'education'),
-(8, 'ssssssssssssssss', 'sssssssssssss', 'sssssssssssssss', '2024-11-08 23:55:22', '2024-11-21 00:00:00', 'ssssssssssssss', 1, 'education'),
-(9, 'AAAAAAAAAAAAAAAAAAAAAAAAAsss', 'ssweeee', 'JJJJJJJJJJJJJJJJJJJJ', '2024-11-09 00:06:42', '2024-11-20 00:00:00', 'weddsdsd', 1, 'educação'),
-(10, 'Bom dia', 'a', 'Boa Noite', '2024-11-09 00:10:04', '2024-11-22 00:00:00', 'b', 1, 'finanças'),
-(11, 'OIIII', 'aaaaaaaaaaaaaaa', 'aaaaaaa', '2024-11-09 00:21:56', '2024-11-23 00:00:00', 'aaaaaaa', 1, 'marketing'),
-(12, 'sdbshvlksd', 'jdfkdjy', 'dugdcmd', '2024-11-09 00:24:18', '2024-11-21 00:00:00', 'dddddddddd', 1, 'música'),
-(14, 'Secretária Sênior', 'Ribeirão Pires', 'Estamos procurando alguém para ser a nossa secretária.\r\nPara essa vaga é necessário saber: Elaborar agenda, excel avançado e conhecimento de arquivo técnico,', '2024-11-09 11:43:22', '2024-12-15 00:00:00', 'SP', 1, 'marketing');
+(1, 'Analista de Marketing', 'Rio de Janeiro', 'Planejar e executar campanhas de marketing digital.', '2024-11-02 09:30:00', '2024-12-02 17:00:00', 'RJ', 1, 'Marketing'),
+(2, 'Contador', 'Belo Horizonte', 'Gerenciar contas e realizar auditorias financeiras.', '2024-11-03 11:00:00', '2024-12-03 18:00:00', 'MG', 1, 'Finanças'),
+(3, 'Designer Gráfico', 'Curitiba', 'Criar materiais gráficos para campanhas publicitárias.', '2024-11-04 08:00:00', '2024-12-04 17:00:00', 'PR', 1, 'Design'),
+(4, 'Professor de Inglês', 'Porto Alegre', 'Ensinar inglês para alunos de nível intermediário.', '2024-11-05 10:00:00', '2024-12-05 18:00:00', 'RS', 1, 'Educação'),
+(5, 'Músico Freelancer', 'Brasília', 'Participar de eventos e gravações.', '2024-11-06 10:00:00', '2024-12-06 18:00:00', 'DF', 1, 'Música'),
 
 --
 -- Índices para tabelas despejadas
