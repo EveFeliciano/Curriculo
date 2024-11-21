14365d844f916e9262cfa5721cdf680fb9445e4f
Gabriel Caspirro Demarchi <124791810+GabrielCaspirro@users.noreply.github.com>
2024-11-20 22:26:59 -0300

    Add files via upload

--- curriculo.sql
+++ curriculo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 09/11/2024 às 04:05
+-- Tempo de geração: 21/11/2024 às 02:21
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -42,11 +42,7 @@ CREATE TABLE `administrador` (
 --
 
 INSERT INTO `administrador` (`id_administrador`, `nome`, `email`, `senha`, `data_cadastro`) VALUES
-(1, 'Evellyn de Santana', 'evellyn@email.com', 'eve123', NULL),
-(2, 'Gabriel Caspirro', 'gabriel@email.com', 'gab123', NULL),
-(3, 'Guilherme Augusto', 'guilhermea@email.com', 'guia123', NULL),
-(4, 'Guilherme Nakamura', 'guilhermen@email.com', 'guin123', NULL),
-(5, 'Raquel Araujo', 'raquela@email.com', 'raq123', NULL);
+(1, 'Gabriel', 'gabriel@email.com', '1234', '2024-11-05 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -62,19 +58,17 @@ CREATE TABLE `candidato` (
   `email` varchar(100) DEFAULT NULL,
   `telefone` varchar(15) DEFAULT NULL,
   `senha` varchar(10) DEFAULT NULL,
-  `data_cadastro` datetime DEFAULT NULL
+  `data_cadastro` datetime DEFAULT NULL,
+  `curriculo` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `candidato`
 --
 
-INSERT INTO `candidato` (`id_candidato`, `nome`, `data_nasc`, `cpf`, `email`, `telefone`, `senha`, `data_cadastro`) VALUES
-(1, 'João Silva', '1990-05-15', '123.456.789', 'joao.silva@email.com', '(11) 91234-5678', 'senha123', NULL),
-(2, 'Maria Oliveira', '1985-11-22', '987.654.321', 'maria.oliveira@email.com', '(21) 99876-5432', 'senha456', NULL),
-(3, 'Carlos Souza', '1992-02-10', '111.222.333', 'carlos.souza@email.com', '(31) 93333-4444', 'senha789', NULL),
-(4, 'Ana Costa', '1988-07-30', '555.666.777', 'ana.costa@email.com', '(41) 94444-5555', 'senha10111', NULL),
-(5, 'Lucas Pereira', '1995-12-05', '333.444.555', 'lucas.pereira@email.com', '(61) 95555-6666', 'senha13141', NULL);
+INSERT INTO `candidato` (`id_candidato`, `nome`, `data_nasc`, `cpf`, `email`, `telefone`, `senha`, `data_cadastro`, `curriculo`) VALUES
+(1, 'Dalva', '2024-11-01', '244.244.343', 'dalva.casp65@gmail.com', '(11) 93031-5532', '1234', '2024-11-05 19:09:58', NULL),
+(8, 'GabrielCD', '2024-07-05', '244.256.343', 'gabriel.demarchi@etec.sp.gov.br', '(11) 93269-8014', '1234', '2024-11-08 20:19:08', 'uploads\\1731108293652.docx');
 
 -- --------------------------------------------------------
 
@@ -91,18 +85,16 @@ CREATE TABLE `candidatura` (
   `curriculo` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Estrutura para tabela `cand_qualidades`
+-- Despejando dados para a tabela `candidatura`
 --
 
-CREATE TABLE `cand_qualidades` (
-  `id_cand_qualidades` int(11) NOT NULL,
-  `outro` varchar(100) DEFAULT NULL,
-  `id_candidatura` int(11) DEFAULT NULL,
-  `id_qualidades` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `candidatura` (`id_candidatura`, `data_candidatura`, `status`, `feedback`, `id_vaga`, `curriculo`) VALUES
+(2, '2024-11-09 00:07:11', 'aprovado', 'respondido', 9, 'dsydkisygvlisc'),
+(3, '2024-11-19 22:15:41', 'aprovado', 'respondido', 6, 'dssssssssssssssssssss'),
+(4, '2024-11-09 00:10:50', 'em espera', NULL, 10, 'aaaaaaaaaaaaa'),
+(5, '2024-11-09 00:14:50', 'reprovado', 'respondido', 9, 'aaaaaaaaa'),
+(6, '2024-11-09 11:45:21', 'aprovado', 'respondido', 14, 'Eu sei desenhar.');
 
 -- --------------------------------------------------------
 
@@ -125,25 +117,8 @@ CREATE TABLE `empresa` (
 --
 
 INSERT INTO `empresa` (`id_empresa`, `nome`, `cnpj`, `telefone`, `email`, `senha`, `data_cadastro`) VALUES
-(1, 'Tech Solutions Ltda.', '12.345.678/000', '(11) 3000-4000', 'contato@techsolutions.com.br', 'empresa123', NULL),
-(2, 'Global Comercio S.A.', '98.765.432/000', '(21) 4000-5000', 'sac@globalcomercio.com.br', 'global456', NULL),
-(3, 'ConstruMax Engenharia', '23.456.789/000', '(31) 5000-6000', 'atendimento@construmax.com.br', 'max789', NULL),
-(4, 'Logística Rápida Ltda.', '34.567.890/000', '(41) 6000-7000', 'logistica@rapida.com.br', 'logistica1', NULL),
-(5, 'Alimentos Naturais S.A.', '45.678.901/000', '(51) 7000-8000', 'contato@alimentosnaturais.com.br', 'alimentos2', NULL);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `entrevista`
---
-
-CREATE TABLE `entrevista` (
-  `id_entrevista` int(11) NOT NULL,
-  `data_entrevista` datetime DEFAULT NULL,
-  `tipo` enum('virtual','presencial') DEFAULT NULL,
-  `localizacao` varchar(100) DEFAULT NULL,
-  `id_candidatura` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+(1, 'Auau', '13.122.122/122', '(11) 93069-8014', 'gabriel.gcd08@gmail.com', '1234', '2024-11-07 17:51:09'),
+(2, 'UIIIII', '13.123.122/122', '(11) 93031-5532', 'gabriel.gc08@gmail.com', '1234', '2024-11-07 18:53:39');
 
 -- --------------------------------------------------------
 
@@ -156,16 +131,16 @@ CREATE TABLE `inscreve` (
   `id_candidato` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Estrutura para tabela `qualidades`
+-- Despejando dados para a tabela `inscreve`
 --
 
-CREATE TABLE `qualidades` (
-  `id_qualidades` int(11) NOT NULL,
-  `descricao` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `inscreve` (`id_candidatura`, `id_candidato`) VALUES
+(2, 8),
+(3, 8),
+(4, 8),
+(5, 8),
+(6, 8);
 
 -- --------------------------------------------------------
 
@@ -177,24 +152,27 @@ CREATE TABLE `vaga` (
   `id_vaga` int(11) NOT NULL,
   `titulo` varchar(100) DEFAULT NULL,
   `cidade` varchar(100) DEFAULT NULL,
-  `estado` varchar(100) DEFAULT NULL,
-  `categorias` varchar(100) DEFAULT NULL,
   `descricao` text DEFAULT NULL,
   `data_publicacao` datetime DEFAULT NULL,
   `data_fechamento` datetime DEFAULT NULL,
-  `id_empresa` int(11) DEFAULT NULL
+  `estado` varchar(100) DEFAULT NULL,
+  `id_empresa` int(11) DEFAULT NULL,
+  `categoria` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `vaga`
 --
 
-INSERT INTO `vaga` (`id_vaga`, `titulo`, `cidade`, `estado`, `categorias`, `descricao`, `data_publicacao`, `data_fechamento`, `id_empresa`) VALUES
-(1, 'Desenvolvedor Backend', 'São Paulo', 'SP', 'Tecnologia', 'Responsável pelo desenvolvimento de soluções backend em sistemas web.', '2024-11-01 00:00:00', '2024-11-30 00:00:00', 1),
-(2, 'Analista de Marketing Digital', 'Rio de Janeiro', 'RJ', 'Marketing', 'Atuação na criação e gestão de campanhas de marketing digital.', '2024-10-25 00:00:00', '2024-11-20 00:00:00', 2),
-(3, 'Engenheiro Civil', 'Belo Horizonte', 'MG', 'Engenharia', 'Responsável por projetos de construção e supervisão de obras.', '2024-11-05 00:00:00', '2024-12-05 00:00:00', 3),
-(4, 'Motorista de Caminhão', 'Curitiba', 'PR', 'Logística', 'Realização de transporte de cargas com caminhão de grande porte.', '2024-11-10 00:00:00', '2024-12-10 00:00:00', 4),
-(5, 'Assistente de RH', 'Porto Alegre', 'RS', 'Recursos Humanos', 'Auxiliar nas atividades de recrutamento, seleção e administração de pessoal.', '2024-11-15 00:00:00', '2024-12-15 00:00:00', 5);
+INSERT INTO `vaga` (`id_vaga`, `titulo`, `cidade`, `descricao`, `data_publicacao`, `data_fechamento`, `estado`, `id_empresa`, `categoria`) VALUES
+(6, 'AngraMoments', 'aaaaaaaaaaaaaaaa', 'aaaaaiosssssssss', '2024-11-07 21:45:15', '2024-11-30 00:00:00', 'aaaaaaaaaaa', 1, 'design'),
+(7, 'escritor', 'ribeirao', 'teurfie7tfieuyfç', '2024-11-07 23:39:28', '2024-11-29 00:00:00', 'sp', 1, 'education'),
+(8, 'ssssssssssssssss', 'sssssssssssss', 'sssssssssssssss', '2024-11-08 23:55:22', '2024-11-21 00:00:00', 'ssssssssssssss', 1, 'education'),
+(9, 'AAAAAAAAAAAAAAAAAAAAAAAAAsss', 'ssweeee', 'JJJJJJJJJJJJJJJJJJJJ', '2024-11-09 00:06:42', '2024-11-20 00:00:00', 'weddsdsd', 1, 'educação'),
+(10, 'Bom dia', 'a', 'Boa Noite', '2024-11-09 00:10:04', '2024-11-22 00:00:00', 'b', 1, 'finanças'),
+(11, 'OIIII', 'aaaaaaaaaaaaaaa', 'aaaaaaa', '2024-11-09 00:21:56', '2024-11-23 00:00:00', 'aaaaaaa', 1, 'marketing'),
+(12, 'sdbshvlksd', 'jdfkdjy', 'dugdcmd', '2024-11-09 00:24:18', '2024-11-21 00:00:00', 'dddddddddd', 1, 'música'),
+(14, 'Secretária Sênior', 'Ribeirão Pires', 'Estamos procurando alguém para ser a nossa secretária.\r\nPara essa vaga é necessário saber: Elaborar agenda, excel avançado e conhecimento de arquivo técnico,', '2024-11-09 11:43:22', '2024-12-15 00:00:00', 'SP', 1, 'marketing');
 
 --
 -- Índices para tabelas despejadas
@@ -223,14 +201,6 @@ ALTER TABLE `candidatura`
   ADD KEY `id_vaga` (`id_vaga`);
 
 --
--- Índices de tabela `cand_qualidades`
---
-ALTER TABLE `cand_qualidades`
-  ADD PRIMARY KEY (`id_cand_qualidades`),
-  ADD KEY `id_candidatura` (`id_candidatura`),
-  ADD KEY `id_qualidades` (`id_qualidades`);
-
---
 -- Índices de tabela `empresa`
 --
 ALTER TABLE `empresa`
@@ -239,24 +209,11 @@ ALTER TABLE `empresa`
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Índices de tabela `entrevista`
---
-ALTER TABLE `entrevista`
-  ADD PRIMARY KEY (`id_entrevista`),
-  ADD KEY `id_candidatura` (`id_candidatura`);
-
---
 -- Índices de tabela `inscreve`
 --
 ALTER TABLE `inscreve`
   ADD PRIMARY KEY (`id_candidatura`,`id_candidato`),
   ADD KEY `id_candidato` (`id_candidato`);
-
---
--- Índices de tabela `qualidades`
---
-ALTER TABLE `qualidades`
-  ADD PRIMARY KEY (`id_qualidades`);
 
 --
 -- Índices de tabela `vaga`
@@ -273,49 +230,31 @@ ALTER TABLE `vaga`
 -- AUTO_INCREMENT de tabela `administrador`
 --
 ALTER TABLE `administrador`
-  MODIFY `id_administrador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_administrador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `candidato`
 --
 ALTER TABLE `candidato`
-  MODIFY `id_candidato` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_candidato` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de tabela `candidatura`
 --
 ALTER TABLE `candidatura`
-  MODIFY `id_candidatura` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `cand_qualidades`
---
-ALTER TABLE `cand_qualidades`
-  MODIFY `id_cand_qualidades` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_candidatura` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `empresa`
 --
 ALTER TABLE `empresa`
-  MODIFY `id_empresa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT de tabela `entrevista`
---
-ALTER TABLE `entrevista`
-  MODIFY `id_entrevista` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `qualidades`
---
-ALTER TABLE `qualidades`
-  MODIFY `id_qualidades` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_empresa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `vaga`
 --
 ALTER TABLE `vaga`
-  MODIFY `id_vaga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_vaga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Restrições para tabelas despejadas
@@ -326,19 +265,6 @@ ALTER TABLE `vaga`
 --
 ALTER TABLE `candidatura`
   ADD CONSTRAINT `candidatura_ibfk_1` FOREIGN KEY (`id_vaga`) REFERENCES `vaga` (`id_vaga`);
-
---
--- Restrições para tabelas `cand_qualidades`
---
-ALTER TABLE `cand_qualidades`
-  ADD CONSTRAINT `cand_qualidades_ibfk_1` FOREIGN KEY (`id_candidatura`) REFERENCES `candidatura` (`id_candidatura`),
-  ADD CONSTRAINT `cand_qualidades_ibfk_2` FOREIGN KEY (`id_qualidades`) REFERENCES `qualidades` (`id_qualidades`);
-
---
--- Restrições para tabelas `entrevista`
---
-ALTER TABLE `entrevista`
-  ADD CONSTRAINT `entrevista_ibfk_1` FOREIGN KEY (`id_candidatura`) REFERENCES `candidatura` (`id_candidatura`);
 
 --
 -- Restrições para tabelas `inscreve`
